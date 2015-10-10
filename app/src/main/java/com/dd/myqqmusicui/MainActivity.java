d70d65c6ebf534eec5c8e6e88a31e9fcf0d80952
东东 <xudong.chen@xgimi.com>
2015-10-10 18:11:48 +0800

    项目初始化

--- app/src/main/java/com/dd/myqqmusicui/MainActivity.java
+++ app/src/main/java/com/dd/myqqmusicui/MainActivity.java
@@ -2,6 +2,8 @@ package com.dd.myqqmusicui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +15,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mViewPager = (ViewPager)findViewById(R.id.main_pager);
+    }
+
+    public void OnIndicatorButtonClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.recommend:
+            case R.id.radio:
+            case R.id.ranking:
+            case R.id.singer:
+        }
     }
 }
